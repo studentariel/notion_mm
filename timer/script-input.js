2552d51b1e8432ecbd31e57d203e7d1bf0d16ac1
somemaker <80939835+studentariel@users.noreply.github.com>
2023-07-04 17:44:19 -0700

    Update script-input.js

--- timer/script-input.js
+++ timer/script-input.js
@@ -1,6 +1,18 @@
 const customTimeInput = document.getElementById('customTime');
 const presetButtons = document.querySelectorAll('.preset');
 const startTimerButton = document.getElementById('startTimer');
+const stopTimerButton = document.getElementById('stopTimer');
+const clearTimerButton = document.getElementById('clearTimer');
+let timerInterval = null;
+
+function startTimer() {
+  const timerDuration = customTimeInput.value;
+  if (timerDuration) {
+    const now = new Date();
+    const endTime = new Date(now.getTime() + timerDuration * 60000);
+    window.location.href = `output.html?endTime=${endTime.toISOString()}`;
+  }
+}
 
 presetButtons.forEach(button => {
   button.addEventListener('click', () => {
@@ -9,11 +21,20 @@ presetButtons.forEach(button => {
 });
 
 startTimerButton.addEventListener('click', () => {
-  const timerDuration = customTimeInput.value;
+  startTimer();
+});
 
-  if (timerDuration) {
-    const now = new Date();
-    const endTime = new Date(now.getTime() + timerDuration * 60000);
-    window.location.href = `output.html?endTime=${endTime.toISOString()}`;
+stopTimerButton.addEventListener('click', () => {
+  if (timerInterval) {
+    clearInterval(timerInterval);
+    timerInterval = null;
   }
+});
+
+clearTimerButton.addEventListener('click', () => {
+  if (timerInterval) {
+    clearInterval(timerInterval);
+    timerInterval = null;
+  }
+  customTimeInput.value = '';
 });
