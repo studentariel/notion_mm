e40c54f3a5367d42726045734871cde2c4cc6489
somemaker <80939835+studentariel@users.noreply.github.com>
2023-07-04 18:55:05 -0700

    Update script.js

--- timer/script.js
+++ timer/script.js
@@ -1,4 +1,6 @@
 const timeOptions = document.querySelectorAll('.time-option');
+const customDurationInput = document.getElementById('custom-duration');
+const setCustomDurationButton = document.getElementById('set-custom-duration');
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
 const clearButton = document.getElementById('clear');
@@ -6,16 +8,31 @@ const backButton = document.getElementById('back');
 const timerDisplay = document.querySelector('.timer-display');
 const timeRemaining = document.getElementById('time-remaining');
 const endTime = document.getElementById('end-time');
+const selectedDurationDisplay = document.getElementById('selected-duration');
 
 let selectedTime = 0;
 let timer;
+
+function displaySelectedDuration() {
+    selectedDurationDisplay.textContent = selectedTime > 0 ? `Selected duration: ${selectedTime} min` : '';
+}
 
 timeOptions.forEach((button) => {
     button.addEventListener('click', () => {
         timeOptions.forEach((btn) => btn.classList.remove('selected'));
         button.classList.add('selected');
         selectedTime = parseInt(button.dataset.time, 10);
+        displaySelectedDuration();
     });
+});
+
+setCustomDurationButton.addEventListener('click', () => {
+    const customDuration = parseInt(customDurationInput.value, 10);
+    if (customDuration > 0) {
+        timeOptions.forEach((btn) => btn.classList.remove('selected'));
+        selectedTime = customDuration;
+        displaySelectedDuration();
+    }
 });
 
 startButton.addEventListener('click', () => {
@@ -27,6 +44,7 @@ startButton.addEventListener('click', () => {
         // Hide timer options and start button
         document.getElementById('timer-options').style.display = 'none';
         startButton.style.display = 'none';
+        selectedDurationDisplay.style.display = 'none';
 
         timer = setInterval(() => {
             const currentTime = new Date();
@@ -60,12 +78,13 @@ backButton.addEventListener('click', () => {
     // Show timer options and start button
     document.getElementById('timer-options').style.display = 'flex';
     startButton.style.display = 'inline-block';
+    selectedDurationDisplay.style.display = 'block';
 });
 
 function formatTime(milliseconds) {
     const minutes = Math.floor(milliseconds / (60 * 1000));
     const seconds = Math.floor((milliseconds % (60 * 1000)) / 1000);
-    return `${minutes}:${seconds < 10 ? '0': ''}${seconds}`;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }
 
 function formatEndTime(date) {
